52cfecb0481a4b735cf65dbc0a4243308899fc1a
SanGit56 <radhiyanrmhhisan@gmail.com>
2025-04-29 13:16:35 +0700

    mengubah file konfigurasi menambahkan file binary & script

---
+++ scripts/cli_lb_ord.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+export FABRIC_CFG_PATH=../config
+export ORDERER_GENERAL_LISTENADDRESS=0.0.0.0
+export ORDERER_GENERAL_LISTENPORT=7050
+export ORDERER_GENERAL_LOCALMSPID=OrdererMSP
+export ORDERER_GENERAL_LOCALMSPDIR=/path/to/crypto/ordererOrganizations/example.com/orderers/orderer.example.com/msp
+
+orderer
+
+
+# channel_name=$1
+
+# export PATH=${ROOTDIR}/../bin:${PWD}/../bin:$PATH
+# export ORDERER_ADMIN_TLS_SIGN_CERT=${PWD}/organizations/ordererOrganizations/example.com/orderers/orderer.example.com/tls/server.crt /dev/null 2>&1
+# export ORDERER_ADMIN_TLS_PRIVATE_KEY=${PWD}/organizations/ordererOrganizations/example.com/orderers/orderer.example.com/tls/server.key /dev/null 2>&1
+
+# osnadmin channel join --channelID ${channel_name} --config-block ./channel-artifacts/${channel_name}.block -o localhost:7053 --ca-file "$ORDERER_CA" --client-cert "$ORDERER_ADMIN_TLS_SIGN_CERT" --client-key "$ORDERER_ADMIN_TLS_PRIVATE_KEY" >> log.txt 2>&1
---
+++ scripts/worker1.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+export FABRIC_CFG_PATH=../config
+export CORE_PEER_ID=peer0.org1.example.com
+export CORE_PEER_ADDRESS=10.125.168.214:7051
+export CORE_PEER_LISTENADDRESS=0.0.0.0:7051
+export CORE_PEER_CHAINCODEADDRESS=10.125.168.214:7052
+export CORE_PEER_CHAINCODELISTENADDRESS=0.0.0.0:7052
+export CORE_PEER_GOSSIP_EXTERNALENDPOINT=10.125.168.214:7051
+export CORE_PEER_LOCALMSPID=Org1MSP
+export CORE_PEER_MSPCONFIGPATH=../organizations/peerOrganizations/org1.example.com/peers/peer0.org1.example.com/msp
+
+peer node start
