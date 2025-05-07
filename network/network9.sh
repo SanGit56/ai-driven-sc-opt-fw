c243aa8efa9e92f797660f5523a011f753e04a8c
SanGit56 <radhiyanrmhhisan@gmail.com>
2025-05-07 17:00:00 +0700

    menyesuaikan: 1. configtx.yaml (dua2nya) 2. network.sh (membuat beberapa versi)
    membuat:
    1. identitas orderer dan peers
    2. genesis block

--- network/network9.sh
+++ network/network9.sh
@@ -4,18 +4,18 @@
 # Customize the paths and IPs based on your actual setup
 
 # Configurable variables
-CHANNEL_NAME="mychannel"
-ORDERER_CA="/path/to/ordererOrganizations/example.com/msp/tlscacerts/tlsca.example.com-cert.pem"
+CHANNEL_NAME="kanal_fabric"
+ORDERER_CA="./organizations/ordererOrganizations/example.com/msp/tlscacerts/tlsca.example.com-cert.pem"
 ORDERER_ADDRESS="orderer.example.com:7050"
-GENESIS_BLOCK_PATH="/path/to/genesis.block"
-CHANNEL_TX_PATH="/path/to/${CHANNEL_NAME}.tx"
+GENESIS_BLOCK_PATH="./system-genesis-block/genesis.block"
+CHANNEL_TX_PATH="./configtx/configtx.yaml"
 
 # Peer configuration
 export CORE_PEER_TLS_ENABLED=true
 export CORE_PEER_LOCALMSPID="Org1MSP"
-export CORE_PEER_TLS_ROOTCERT_FILE="/path/to/org1/peers/peer0.org1.example.com/tls/ca.crt"
-export CORE_PEER_MSPCONFIGPATH="/path/to/org1/users/Admin@org1.example.com/msp"
-export CORE_PEER_ADDRESS="peer0.org1.example.com:7051"
+export CORE_PEER_TLS_ROOTCERT_FILE="/etc/hyperledger/fabric/tls/ca.crt"
+export CORE_PEER_MSPCONFIGPATH="/etc/hyperledger/fabric/msp"
+export CORE_PEER_ADDRESS="peer1.org1.example.com:8051"
 
 function createChannel() {
     echo "Creating channel $CHANNEL_NAME..."
