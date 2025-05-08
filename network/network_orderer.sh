e48de3fc3cb8a52962a8cc68df9497005f3ef383
SanGit56 <radhiyanrmhhisan@gmail.com>
2025-05-08 09:35:06 +0700

    menambahkan komentar pada skrip network khusus

--- network/network_orderer.sh
+++ network/network_orderer.sh
@@ -1,5 +1,11 @@
+# simpul orderer
 cryptogen generate --config=./organizations/cryptogen/crypto-config-orderer.yaml --output="organizations"
 
+# blok genesis
 configtxgen -profile ChannelUsingRaft -outputBlock ./channel-artifacts/kanal_fabric.block -channelID kanal_fabric
 
+# orderer buat saluran
 configtxgen -profile ChannelUsingRaft -outputCreateChannelTx ./channel-artifacts/kanal_fabric.tx -channelID kanal_fabric
+
+# orderer gabung saluran
+scripts/orderer.sh
--- network/network_peers.sh
+++ network/network_peers.sh
@@ -1,9 +1,11 @@
+# simpul peer
 cryptogen generate --config=./organizations/cryptogen/crypto-config-org1.yaml --output="organizations"
 
-scripts/orderer.sh
-
+# peer buat saluran
 peer channel create -o orderer.example.com:7050 --ordererTLSHostnameOverride orderer.example.com -c kanal_fabric -f ./channel-artifacts/kanal_fabric.tx --outputBlock ./channel-artifacts/kanal_fabric.block --tls --cafile /path/to/orderer/ca.crt
 
+# peer gabung saluran
 peer channel join -b ./channel-artifacts/kanal_fabric.block
 
+# buat anchor peer
 peer channel update -o orderer.example.com:7050 --ordererTLSHostnameOverride orderer.example.com -c kanal_fabric -f ./channel-artifacts/Org1MSPanchors.tx --tls --cafile /path/to/orderer/ca.crt
