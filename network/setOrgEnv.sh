b012be3aa75f49e8c3a3f3801894043eaf650a43
SanGit56 <radhiyanrmhhisan@gmail.com>
2025-05-14 09:28:26 +0700

    menambahkan alamat variabel lingkungan

--- network/setOrgEnv.sh
+++ network/setOrgEnv.sh
@@ -15,31 +15,48 @@ ORG_LOWER=$(echo "$ORG" | tr '[:upper:]' '[:lower:]')
 if [[ "$ORG_LOWER" == "org1" || "$ORG_LOWER" == "digibank" ]]; then
 
   export CORE_PEER_LOCALMSPID="Org1MSP"
-  export CORE_PEER_MSPCONFIGPATH="${DIR}/network/organizations/peerOrganizations/org1.example.com/users/Admin@org1.example.com/msp"
+  export PEER_TLS_CLIENT_ROOTCAs_FILE="${DIR}/network/organizations/peerOrganizations/org1.example.com/users/Admin@org1.example.com/tls/ca.crt"
+  export PEER_TLS_CLIENT_KEY_FILE="${DIR}/network/organizations/peerOrganizations/org1.example.com/users/Admin@org1.example.com/tls/client.key"
+  export PEER_TLS_CLIENT_CERT_FILE="${DIR}/network/organizations/peerOrganizations/org1.example.com/users/Admin@org1.example.com/tls/client.crt"
 
   case "$PEER" in
     0)
-        export CORE_PEER_ADDRESS="10.125.169.93:7051"
-        export CORE_PEER_TLS_ROOTCERT_FILE="$PEER0_ORG1_CA"
-        ;;
+      export PEER_ID=peer0.org1.example.com
+      export CORE_PEER_ADDRESS="10.125.169.93:7051"
+      export PEER_GOSSIP_BOOTSTRAP=10.125.169.12:8051
+      export CORE_PEER_MSPCONFIGPATH="${DIR}/network/organizations/peerOrganizations/org1.example.com/peers/$PEER_ID/msp"
+      export PEER_TLS_CERT_FILE="${DIR}/network/organizations/peerOrganizations/org1.example.com/peers/$PEER_ID/tls/server.crt"
+      export PEER_TLS_KEY_FILE=="${DIR}/network/organizations/peerOrganizations/org1.example.com/peers/$PEER_ID/tls/server.key"
+      export CORE_PEER_TLS_ROOTCERT_FILE="$PEER0_ORG1_CA"
+      ;;
     1)
-        export CORE_PEER_ADDRESS="10.125.169.12:8051"
-        export CORE_PEER_TLS_ROOTCERT_FILE="$PEER1_ORG1_CA"
-        ;;
+      export PEER_ID=peer1.org1.example.com
+      export CORE_PEER_ADDRESS="10.125.169.12:8051"
+      export PEER_GOSSIP_BOOTSTRAP=10.125.169.6:9051
+      export CORE_PEER_MSPCONFIGPATH="${DIR}/network/organizations/peerOrganizations/org1.example.com/peers/$PEER_ID/msp"
+      export PEER_TLS_CERT_FILE="${DIR}/network/organizations/peerOrganizations/org1.example.com/peers/$PEER_ID/tls/server.crt"
+      export PEER_TLS_KEY_FILE=="${DIR}/network/organizations/peerOrganizations/org1.example.com/peers/$PEER_ID/tls/server.key"
+      export CORE_PEER_TLS_ROOTCERT_FILE="$PEER1_ORG1_CA"
+      ;;
     2)
-        export CORE_PEER_ADDRESS="10.125.169.6:9051"
-        export CORE_PEER_TLS_ROOTCERT_FILE="$PEER2_ORG1_CA"
-        ;;
+      export PEER_ID=peer2.org1.example.com
+      export CORE_PEER_ADDRESS="10.125.169.6:9051"
+      export PEER_GOSSIP_BOOTSTRAP=10.125.169.93:7051
+      export CORE_PEER_MSPCONFIGPATH="${DIR}/network/organizations/peerOrganizations/org1.example.com/peers/$PEER_ID/msp"
+      export PEER_TLS_CERT_FILE="${DIR}/network/organizations/peerOrganizations/org1.example.com/peers/$PEER_ID/tls/server.crt"
+      export PEER_TLS_KEY_FILE=="${DIR}/network/organizations/peerOrganizations/org1.example.com/peers/$PEER_ID/tls/server.key"
+      export CORE_PEER_TLS_ROOTCERT_FILE="$PEER2_ORG1_CA"
+      ;;
     *)
-        echo "Error: Peer${PEER} in Org1 is not recognized."
-        return 1
-        ;;
+      echo "Error: Peer${PEER} in Org1 is not recognized."
+      return 1
+      ;;
   esac
 else
-    echo "Unknown organization \"$ORG\"."
-    echo "Please choose from: Org1, Digibank"
-    echo "Usage: source ./setOrgEnv.sh Org1 0"
-    return 1
+  echo "Unknown organization \"$ORG\"."
+  echo "Please choose from: Org1, Digibank"
+  echo "Usage: source ./setOrgEnv.sh Org1 0"
+  return 1
 fi
 
 # Common exports
