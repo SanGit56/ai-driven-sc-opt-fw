fc9e6cc61bf1163129db6510c1a608fd98f1586b
SanGit56 <radhiyanrmhhisan@gmail.com>
2025-05-17 12:29:36 +0700

    mengubah alamat menjadi absolut memisahkan core.yaml untuk tiap mesin

--- network/network_orderer.sh
+++ network/network_orderer.sh
@@ -1,26 +1,26 @@
 export PATH="$PATH:$(pwd)/bin"
 
 # generate orderer identity
-cryptogen generate --config=./network/organizations/cryptogen/crypto-config-orderer.yaml --output="network/organizations"
+cryptogen generate --config=~/go/src/github.com/SanGit56/ai-driven-sc-opt-fw/network/organizations/cryptogen/crypto-config-orderer.yaml --output="~/go/src/github.com/SanGit56/ai-driven-sc-opt-fw/network/organizations"
 
 # generate organization/peer identity
-cryptogen generate --config=./network/organizations/cryptogen/crypto-config-org1.yaml --output="network/organizations"
+cryptogen generate --config=~/go/src/github.com/SanGit56/ai-driven-sc-opt-fw/network/organizations/cryptogen/crypto-config-org1.yaml --output="~/go/src/github.com/SanGit56/ai-driven-sc-opt-fw/network/organizations"
 
 export FABRIC_CFG_PATH="$PWD/config"
 
 # generate genesis block
-configtxgen -profile SampleDevModeEtcdRaft -outputBlock ./config/system-genesis-block/genesis.block -channelID system-channel
+configtxgen -profile SampleDevModeEtcdRaft -outputBlock ~/go/src/github.com/SanGit56/ai-driven-sc-opt-fw/config/system-genesis-block/genesis.block -channelID system-channel
 
 export FABRIC_CFG_PATH="$PWD/network/configtx"
 
 # generate app genesis/config block
-configtxgen -profile ChannelUsingRaft -outputBlock ./network/configtx/channel-artifacts/kanal-fabric.block -channelID kanal-fabric
+configtxgen -profile ChannelUsingRaft -outputBlock ~/go/src/github.com/SanGit56/ai-driven-sc-opt-fw/network/configtx/channel-artifacts/kanal-fabric.block -channelID kanal-fabric
 
 # generate channel
-configtxgen -profile ChannelUsingRaft -outputCreateChannelTx ./network/configtx/channel-artifacts/kanal-fabric.tx -channelID kanal-fabric
+configtxgen -profile ChannelUsingRaft -outputCreateChannelTx ~/go/src/github.com/SanGit56/ai-driven-sc-opt-fw/network/configtx/channel-artifacts/kanal-fabric.tx -channelID kanal-fabric
 
 # update anchor peer transaction
-configtxgen -profile ChannelUsingRaft -outputAnchorPeersUpdate ./network/configtx/channel-artifacts/Org1MSPanchors.tx -channelID kanal-fabric -asOrg Org1
+configtxgen -profile ChannelUsingRaft -outputAnchorPeersUpdate ~/go/src/github.com/SanGit56/ai-driven-sc-opt-fw/network/configtx/channel-artifacts/Org1MSPanchors.tx -channelID kanal-fabric -asOrg Org1
 
 cd config
 
