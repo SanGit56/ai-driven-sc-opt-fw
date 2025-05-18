2777002b9affda620a1f2376f9864dabd04d4c0c
SanGit56 <radhiyanrmhhisan@gmail.com>
2025-05-18 13:58:21 +0700

    memperbaiki alamat file

--- network/network_peers.sh
+++ network/network_peers.sh
@@ -3,10 +3,10 @@ source ./network/setOrgEnv.sh Org1 2
 cd config
 
 # create config block
-peer channel create -o 10.125.169.102:7050 --ordererTLSHostnameOverride orderer.example.com -c kanal-fabric -f ~/go/src/github.com/SanGit56/ai-driven-sc-opt-fw/network/configtx/channel-artifacts/kanal-fabric.tx --outputBlock ~/go/src/github.com/SanGit56/ai-driven-sc-opt-fw/network/configtx/channel-artifacts/kanal-fabric.block --tls --cafile ~/go/src/github.com/SanGit56/ai-driven-sc-opt-fw/network/organizations/ordererOrganizations/example.com/tlsca/tlsca.example.com-cert.pem
+peer channel create -o 10.125.169.102:7050 --ordererTLSHostnameOverride orderer.example.com -c kanal-fabric -f /home/loadbalancer/go/src/github.com/SanGit56/ai-driven-sc-opt-fw/network/configtx/channel-artifacts/kanal-fabric.tx --outputBlock /home/loadbalancer/go/src/github.com/SanGit56/ai-driven-sc-opt-fw/network/configtx/channel-artifacts/kanal-fabric.block --tls --cafile /home/loadbalancer/go/src/github.com/SanGit56/ai-driven-sc-opt-fw/network/organizations/ordererOrganizations/example.com/tlsca/tlsca.example.com-cert.pem
 
 # join channel
-peer channel join -b ~/go/src/github.com/SanGit56/ai-driven-sc-opt-fw/network/configtx/channel-artifacts/kanal-fabric.block
+peer channel join -b /home/loadbalancer/go/src/github.com/SanGit56/ai-driven-sc-opt-fw/network/configtx/channel-artifacts/kanal-fabric.block
 
 # update peers
-peer channel update -o 10.125.169.102:7050 --ordererTLSHostnameOverride orderer.example.com -c kanal-fabric -f ~/go/src/github.com/SanGit56/ai-driven-sc-opt-fw/network/configtx/channel-artifacts/Org1MSPanchors.tx --tls --cafile ~/go/src/github.com/SanGit56/ai-driven-sc-opt-fw/network/organizations/ordererOrganizations/example.com/tlsca/tlsca.example.com-cert.pem
+peer channel update -o 10.125.169.102:7050 --ordererTLSHostnameOverride orderer.example.com -c kanal-fabric -f /home/loadbalancer/go/src/github.com/SanGit56/ai-driven-sc-opt-fw/network/configtx/channel-artifacts/Org1MSPanchors.tx --tls --cafile /home/loadbalancer/go/src/github.com/SanGit56/ai-driven-sc-opt-fw/network/organizations/ordererOrganizations/example.com/tlsca/tlsca.example.com-cert.pem
--- network/setOrgEnv.sh
+++ network/setOrgEnv.sh
@@ -5,44 +5,44 @@ PEER=${2:-0}
 
 DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd)"
 
-ORDERER_CA="~/go/src/github.com/SanGit56/ai-driven-sc-opt-fw/network/organizations/ordererOrganizations/example.com/orderers/orderer.example.com/tls/ca.crt"
-PEER0_ORG1_CA="~/go/src/github.com/SanGit56/ai-driven-sc-opt-fw/network/organizations/peerOrganizations/org1.example.com/peers/peer0.org1.example.com/tls/ca.crt"
-PEER1_ORG1_CA="~/go/src/github.com/SanGit56/ai-driven-sc-opt-fw/network/organizations/peerOrganizations/org1.example.com/peers/peer1.org1.example.com/tls/ca.crt"
-PEER2_ORG1_CA="~/go/src/github.com/SanGit56/ai-driven-sc-opt-fw/network/organizations/peerOrganizations/org1.example.com/peers/peer2.org1.example.com/tls/ca.crt"
+ORDERER_CA="/home/loadbalancer/go/src/github.com/SanGit56/ai-driven-sc-opt-fw/network/organizations/ordererOrganizations/example.com/orderers/orderer.example.com/tls/ca.crt"
+PEER0_ORG1_CA="/home/loadbalancer/go/src/github.com/SanGit56/ai-driven-sc-opt-fw/network/organizations/peerOrganizations/org1.example.com/peers/peer0.org1.example.com/tls/ca.crt"
+PEER1_ORG1_CA="/home/loadbalancer/go/src/github.com/SanGit56/ai-driven-sc-opt-fw/network/organizations/peerOrganizations/org1.example.com/peers/peer1.org1.example.com/tls/ca.crt"
+PEER2_ORG1_CA="/home/loadbalancer/go/src/github.com/SanGit56/ai-driven-sc-opt-fw/network/organizations/peerOrganizations/org1.example.com/peers/peer2.org1.example.com/tls/ca.crt"
 
 ORG_LOWER=$(echo "$ORG" | tr '[:upper:]' '[:lower:]')
 
 if [[ "$ORG_LOWER" == "org1" || "$ORG_LOWER" == "digibank" ]]; then
 
   export CORE_PEER_LOCALMSPID="Org1MSP"
-  export PEER_TLS_CLIENT_ROOTCAs_FILE="~/go/src/github.com/SanGit56/ai-driven-sc-opt-fw/network/ordererOrganizations/example.com/orderers/orderer.example.com/tls/ca.crt"
-  export PEER_TLS_CLIENT_KEY_FILE="~/go/src/github.com/SanGit56/ai-driven-sc-opt-fw/network/organizations/peerOrganizations/org1.example.com/users/Admin@org1.example.com/tls/client.key"
-  export PEER_TLS_CLIENT_CERT_FILE="~/go/src/github.com/SanGit56/ai-driven-sc-opt-fw/network/organizations/peerOrganizations/org1.example.com/users/Admin@org1.example.com/tls/client.crt"
-  export CORE_PEER_MSPCONFIGPATH="~/go/src/github.com/SanGit56/ai-driven-sc-opt-fw/network/organizations/peerOrganizations/org1.example.com/users/Admin@org1.example.com/msp"
+  export PEER_TLS_CLIENT_ROOTCAs_FILE="/home/loadbalancer/go/src/github.com/SanGit56/ai-driven-sc-opt-fw/network/ordererOrganizations/example.com/orderers/orderer.example.com/tls/ca.crt"
+  export PEER_TLS_CLIENT_KEY_FILE="/home/loadbalancer/go/src/github.com/SanGit56/ai-driven-sc-opt-fw/network/organizations/peerOrganizations/org1.example.com/users/Admin@org1.example.com/tls/client.key"
+  export PEER_TLS_CLIENT_CERT_FILE="/home/loadbalancer/go/src/github.com/SanGit56/ai-driven-sc-opt-fw/network/organizations/peerOrganizations/org1.example.com/users/Admin@org1.example.com/tls/client.crt"
+  export CORE_PEER_MSPCONFIGPATH="/home/loadbalancer/go/src/github.com/SanGit56/ai-driven-sc-opt-fw/network/organizations/peerOrganizations/org1.example.com/users/Admin@org1.example.com/msp"
 
   case "$PEER" in
     0)
       export PEER_ID=peer0.org1.example.com
       export CORE_PEER_ADDRESS="10.125.169.93:7051"
       export PEER_GOSSIP_BOOTSTRAP=10.125.169.12:8051
-      export PEER_TLS_CERT_FILE="~/go/src/github.com/SanGit56/ai-driven-sc-opt-fw/network/organizations/peerOrganizations/org1.example.com/peers/$PEER_ID/tls/server.crt"
-      export PEER_TLS_KEY_FILE=="~/go/src/github.com/SanGit56/ai-driven-sc-opt-fw/network/organizations/peerOrganizations/org1.example.com/peers/$PEER_ID/tls/server.key"
+      export PEER_TLS_CERT_FILE="/home/loadbalancer/go/src/github.com/SanGit56/ai-driven-sc-opt-fw/network/organizations/peerOrganizations/org1.example.com/peers/$PEER_ID/tls/server.crt"
+      export PEER_TLS_KEY_FILE=="/home/loadbalancer/go/src/github.com/SanGit56/ai-driven-sc-opt-fw/network/organizations/peerOrganizations/org1.example.com/peers/$PEER_ID/tls/server.key"
       export CORE_PEER_TLS_ROOTCERT_FILE="$PEER0_ORG1_CA"
       ;;
     1)
       export PEER_ID=peer1.org1.example.com
       export CORE_PEER_ADDRESS="10.125.169.12:8051"
       export PEER_GOSSIP_BOOTSTRAP=10.125.169.6:9051
-      export PEER_TLS_CERT_FILE="~/go/src/github.com/SanGit56/ai-driven-sc-opt-fw/network/organizations/peerOrganizations/org1.example.com/peers/$PEER_ID/tls/server.crt"
-      export PEER_TLS_KEY_FILE=="~/go/src/github.com/SanGit56/ai-driven-sc-opt-fw/network/organizations/peerOrganizations/org1.example.com/peers/$PEER_ID/tls/server.key"
+      export PEER_TLS_CERT_FILE="/home/loadbalancer/go/src/github.com/SanGit56/ai-driven-sc-opt-fw/network/organizations/peerOrganizations/org1.example.com/peers/$PEER_ID/tls/server.crt"
+      export PEER_TLS_KEY_FILE=="/home/loadbalancer/go/src/github.com/SanGit56/ai-driven-sc-opt-fw/network/organizations/peerOrganizations/org1.example.com/peers/$PEER_ID/tls/server.key"
       export CORE_PEER_TLS_ROOTCERT_FILE="$PEER1_ORG1_CA"
       ;;
     2)
       export PEER_ID=peer2.org1.example.com
       export CORE_PEER_ADDRESS="10.125.169.6:9051"
       export PEER_GOSSIP_BOOTSTRAP=10.125.169.93:7051
-      export PEER_TLS_CERT_FILE="~/go/src/github.com/SanGit56/ai-driven-sc-opt-fw/network/organizations/peerOrganizations/org1.example.com/peers/$PEER_ID/tls/server.crt"
-      export PEER_TLS_KEY_FILE=="~/go/src/github.com/SanGit56/ai-driven-sc-opt-fw/network/organizations/peerOrganizations/org1.example.com/peers/$PEER_ID/tls/server.key"
+      export PEER_TLS_CERT_FILE="/home/loadbalancer/go/src/github.com/SanGit56/ai-driven-sc-opt-fw/network/organizations/peerOrganizations/org1.example.com/peers/$PEER_ID/tls/server.crt"
+      export PEER_TLS_KEY_FILE=="/home/loadbalancer/go/src/github.com/SanGit56/ai-driven-sc-opt-fw/network/organizations/peerOrganizations/org1.example.com/peers/$PEER_ID/tls/server.key"
       export CORE_PEER_TLS_ROOTCERT_FILE="$PEER2_ORG1_CA"
       ;;
     *)
