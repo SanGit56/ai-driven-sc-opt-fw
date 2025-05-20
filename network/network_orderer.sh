77dee548b408b731e9929274bc2001ec005b9f9d
SanGit56 <radhiyanrmhhisan@gmail.com>
2025-05-20 14:32:02 +0700

    menghapus: 1. blok genesis (kanal sistem) 2. menghapus consortium
    mengubah:
    1. alamat sertifikat Admin
    2. skrip jaringan

--- network/network_orderer.sh
+++ network/network_orderer.sh
@@ -1,35 +1,29 @@
-export PATH="$PATH:$(pwd)/bin"
+export PATH="$PATH:$(pwd)/../bin"
 
 # generate orderer identity
-cryptogen generate --config=./network/organizations/cryptogen/crypto-config-orderer.yaml --output="network/organizations"
+cryptogen generate --config=./organizations/cryptogen/crypto-config-orderer.yaml --output="./organizations"
 
 # generate organization/peer identity
-cryptogen generate --config=./network/organizations/cryptogen/crypto-config-org1.yaml --output="network/organizations"
+cryptogen generate --config=./organizations/cryptogen/crypto-config-org1.yaml --output="./organizations"
 
-# salin orderer & peer identity ke peer
-
-export FABRIC_CFG_PATH="$PWD/config"
-
-# generate genesis block
-configtxgen -profile SampleDevModeEtcdRaft -outputBlock ./config/system-genesis-block/genesis.block -channelID system-channel
-
-export FABRIC_CFG_PATH="$PWD/network/configtx"
+export FABRIC_CFG_PATH="$PWD/configtx"
 
 # generate app genesis/config block
-configtxgen -profile ChannelUsingRaft -outputBlock ./network/configtx/channel-artifacts/kanal-fabric.block -channelID kanal-fabric
+configtxgen -profile ChannelUsingRaft -outputBlock ./configtx/channel-artifacts/kanal-fabric.block -channelID kanal-fabric
 
-# generate channel
-configtxgen -profile ChannelUsingRaft -outputCreateChannelTx ./network/configtx/channel-artifacts/kanal-fabric.tx -channelID kanal-fabric
-
-# update anchor peer transaction
-configtxgen -profile ChannelUsingRaft -outputAnchorPeersUpdate ./network/configtx/channel-artifacts/Org1MSPanchors.tx -channelID kanal-fabric -asOrg Org1
-
-cd config
-
-# run orderer
 orderer
-
-cd ../network
 
 # orderer script (setelah create config block)
 scripts/orderer.sh
+
+
+# update anchor peer transaction
+configtxgen -profile ChannelUsingRaft -outputAnchorPeersUpdate /configtx/channel-artifacts/Org1MSPanchors.tx -channelID kanal-fabric -asOrg Org1
+
+
+
+# generate genesis block
+# configtxgen -profile SampleDevModeEtcdRaft -outputBlock ./config/system-genesis-block/genesis.block -channelID system-channel
+
+# generate channel
+# configtxgen -profile ChannelUsingRaft -outputCreateChannelTx ./configtx/channel-artifacts/kanal-fabric.tx -channelID kanal-fabric
--- network/network_peers.sh
+++ network/network_peers.sh
@@ -1,12 +1,14 @@
-export PATH="$PATH:$(pwd)/bin"
-source ./network/setOrgEnv.sh Org1 2
-cd config
-
-# create config block
-peer channel create -o 10.125.169.102:7050 --ordererTLSHostnameOverride orderer.example.com -c kanal-fabric -f ../network/configtx/channel-artifacts/kanal-fabric.tx --outputBlock ../network/configtx/channel-artifacts/kanal-fabric.block --tls --cafile ../network/organizations/ordererOrganizations/example.com/tlsca/tlsca.example.com-cert.pem
+export PATH="$PATH:$(pwd)/../bin"
+peer node start
 
 # join channel
 peer channel join -b ../network/configtx/channel-artifacts/kanal-fabric.block
 
+
 # update peers
 peer channel update -o 10.125.169.102:7050 --ordererTLSHostnameOverride orderer.example.com -c kanal-fabric -f ../network/configtx/channel-artifacts/Org1MSPanchors.tx --tls --cafile ../network/organizations/ordererOrganizations/example.com/tlsca/tlsca.example.com-cert.pem
+
+
+
+# create config block
+# peer channel create -o 10.125.169.102:7050 --ordererTLSHostnameOverride orderer.example.com -c kanal-fabric -f ../network/configtx/channel-artifacts/kanal-fabric.tx --outputBlock ../network/configtx/channel-artifacts/kanal-fabric.block --tls --cafile ../network/organizations/ordererOrganizations/example.com/tlsca/tlsca.example.com-cert.pem
