ece1654e877aac04c4d6e9e66a1fd4d1440e6422
SanGit56 <radhiyanrmhhisan@gmail.com>
2025-05-21 16:56:21 +0700

    mengubah port listenAddress & menambah perintah export

--- network/network_peers.sh
+++ network/network_peers.sh
@@ -1,6 +1,7 @@
 export PATH="$PATH:$(pwd)/../bin"
 peer node start
 
+export CORE_PEER_MSPCONFIGPATH=../network/organizations/peerOrganizations/org1.example.com/users/Admin@org1.example.com/msp
 # join channel
 peer channel join -b ../network/configtx/channel-artifacts/kanal-fabric.block
 
