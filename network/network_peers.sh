e77115ae5d9011fc130b43f0d786067e21bb80de
SanGit56 <radhiyanrmhhisan@gmail.com>
2025-05-28 14:36:11 +0700

    ip orderer berubah lagi

--- network/network_peers.sh
+++ network/network_peers.sh
@@ -6,13 +6,13 @@ export CORE_PEER_MSPCONFIGPATH=../network/organizations/peerOrganizations/org1.e
 peer channel join -b ../network/configtx/channel-artifacts/kanal-fabric.block
 
 # install chaincode on peer
-scripts/deployCC_peers.sh kanal-fabric basic ../asset-transfer-basic/chaincode-go go
+scripts/deployCC_peers.sh kanal-fabric basic
 
 
 # update peers
-peer channel update -o 10.125.169.226:7050 --ordererTLSHostnameOverride orderer.example.com -c kanal-fabric -f ../network/configtx/channel-artifacts/Org1MSPanchors.tx --tls --cafile ../network/organizations/ordererOrganizations/example.com/tlsca/tlsca.example.com-cert.pem
+peer channel update -o 10.125.170.118:7050 --ordererTLSHostnameOverride orderer.example.com -c kanal-fabric -f ../network/configtx/channel-artifacts/Org1MSPanchors.tx --tls --cafile ../network/organizations/ordererOrganizations/example.com/tlsca/tlsca.example.com-cert.pem
 
 
 
 # create config block
-# peer channel create -o 10.125.169.226:7050 --ordererTLSHostnameOverride orderer.example.com -c kanal-fabric -f ../network/configtx/channel-artifacts/kanal-fabric.tx --outputBlock ../network/configtx/channel-artifacts/kanal-fabric.block --tls --cafile ../network/organizations/ordererOrganizations/example.com/tlsca/tlsca.example.com-cert.pem
+# peer channel create -o 10.125.170.118:7050 --ordererTLSHostnameOverride orderer.example.com -c kanal-fabric -f ../network/configtx/channel-artifacts/kanal-fabric.tx --outputBlock ../network/configtx/channel-artifacts/kanal-fabric.block --tls --cafile ../network/organizations/ordererOrganizations/example.com/tlsca/tlsca.example.com-cert.pem
--- network/scripts/deployCC_peers.sh
+++ network/scripts/deployCC_peers.sh
@@ -4,48 +4,10 @@ source scripts/utils.sh
 
 CHANNEL_NAME=${1:-"kanal-fabric"}
 CC_NAME=${2}
-CC_SRC_PATH=${3}
-CC_SRC_LANGUAGE=${4}
-CC_VERSION=${5:-"1.0"}
-CC_SEQUENCE=${6:-"1"}
-CC_INIT_FCN=${7:-"NA"}
-CC_END_POLICY=${8:-"NA"}
-CC_COLL_CONFIG=${9:-"NA"}
-DELAY=${10:-"3"}
-MAX_RETRY=${11:-"5"}
-VERBOSE=${12:-"false"}
 
 println "executing with the following"
 println "- CHANNEL_NAME: ${C_GREEN}${CHANNEL_NAME}${C_RESET}"
 println "- CC_NAME: ${C_GREEN}${CC_NAME}${C_RESET}"
-println "- CC_SRC_PATH: ${C_GREEN}${CC_SRC_PATH}${C_RESET}"
-println "- CC_SRC_LANGUAGE: ${C_GREEN}${CC_SRC_LANGUAGE}${C_RESET}"
-println "- CC_VERSION: ${C_GREEN}${CC_VERSION}${C_RESET}"
-println "- CC_SEQUENCE: ${C_GREEN}${CC_SEQUENCE}${C_RESET}"
-println "- CC_END_POLICY: ${C_GREEN}${CC_END_POLICY}${C_RESET}"
-println "- CC_COLL_CONFIG: ${C_GREEN}${CC_COLL_CONFIG}${C_RESET}"
-println "- CC_INIT_FCN: ${C_GREEN}${CC_INIT_FCN}${C_RESET}"
-println "- DELAY: ${C_GREEN}${DELAY}${C_RESET}"
-println "- MAX_RETRY: ${C_GREEN}${MAX_RETRY}${C_RESET}"
-println "- VERBOSE: ${C_GREEN}${VERBOSE}${C_RESET}"
-
-INIT_REQUIRED="--init-required"
-
-if [ "$CC_INIT_FCN" = "NA" ]; then
-  INIT_REQUIRED=""
-fi
-
-if [ "$CC_END_POLICY" = "NA" ]; then
-  CC_END_POLICY=""
-else
-  CC_END_POLICY="--signature-policy $CC_END_POLICY"
-fi
-
-if [ "$CC_COLL_CONFIG" = "NA" ]; then
-  CC_COLL_CONFIG=""
-else
-  CC_COLL_CONFIG="--collections-config $CC_COLL_CONFIG"
-fi
 
 export FABRIC_CFG_PATH=$PWD/../config/
 
@@ -68,7 +30,7 @@ checkPrereqs
 
 PACKAGE_ID=$(peer lifecycle chaincode calculatepackageid ${CC_NAME}.tar.gz)
 
-infoln "Installing chaincode on peer0.org1..."
+infoln "Installing chaincode on peer.org1..."
 installChaincode 1
 
 # resolveSequence
