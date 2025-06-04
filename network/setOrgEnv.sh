bdb7df08df432033e2f9c981cc21126cf28bf121
SanGit56 <radhiyanrmhhisan@gmail.com>
2025-06-04 13:25:33 +0700

    ip peer0 & peer1 berubah lagi

--- network/setOrgEnv.sh
+++ network/setOrgEnv.sh
@@ -18,10 +18,10 @@ if [[ "$ORG_LOWER" == "org1" || "$ORG_LOWER" == "digibank" ]]; then
 
   case "$PEER" in
     0)
-      export CORE_PEER_ADDRESS="10.125.170.255:7051"
+      export CORE_PEER_ADDRESS="10.125.174.252:7051"
       ;;
     1)
-      export CORE_PEER_ADDRESS="10.125.171.43:8051"
+      export CORE_PEER_ADDRESS="10.125.175.114:8051"
       ;;
     2)
       export CORE_PEER_ADDRESS="10.125.171.70:9051"
