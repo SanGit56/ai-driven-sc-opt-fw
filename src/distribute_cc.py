5ceac132be13f1a57a1cf81d0571d407a2953ebf
SanGit56 <radhiyanrmhhisan@gmail.com>
2025-06-12 17:25:46 +0700

    distribute_cc.py masih belum bisa

--- src/distribute_cc.py
+++ src/distribute_cc.py
@@ -8,9 +8,9 @@ def kirim_cc_sesuai_rekom(file_rekom, ssh_kunci):
       if '->' not in baris:
         continue
 
-      nama_cc, daftar_ip = baris.strip().split('->')
+      nama_cc, daftar_peer = baris.strip().split('->')
       nama_cc = nama_cc.strip()
-      ip_peer = [ip.strip() for ip in daftar_ip.split(',')]
+      nama_peer = [peer.strip() for peer in daftar_peer.split(',')]
 
       alamat_cc_terkemas = os.path.join('../packaged_cc', f'{nama_cc}.tar.gz')
 
@@ -18,33 +18,37 @@ def kirim_cc_sesuai_rekom(file_rekom, ssh_kunci):
         print(f'{alamat_cc_terkemas} tidak ditemukan')
         continue
 
-      for ip in ip_peer:
+      for peer in nama_peer:
         try:
-          if ip == "10.125.176.28":
+          if peer == "peer0":
             ssh_pengguna = "worker1"
-          elif ip == "10.125.176.79":
+            ip = "10.125.176.28"
+          elif peer == "peer1":
             ssh_pengguna = "worker2"
-          elif ip == "10.125.175.105":
+            ip = "10.125.176.79"
+          elif peer == "peer2":
             ssh_pengguna = "worker3"
+            ip = "10.125.175.105"
           else:
             ssh_pengguna = "workerx"
+            ip = "10.125.xxx.xxx"
 
-          print(f'Mengirim {nama_cc}.tar.gz to {ip}')
+          print(f'Mengirim {nama_cc}.tar.gz to {peer}')
 
           ssh = paramiko.SSHClient()
           ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
           ssh.connect(hostname=ip, username=ssh_pengguna, key_filename=f'/home/{ssh_pengguna}/.ssh/id_rsa')
 
           with SCPClient(ssh.get_transport()) as scp:
-            scp.put(alamat_cc_terkemas, remote_path=f'home/{ssh_pengguna}/ai-driven-sc-opt-fw/packaged_cc')
+            scp.put(alamat_cc_terkemas, remote_path=f'/home/{ssh_pengguna}/ai-driven-sc-opt-fw/packaged_cc')
 
           ssh.close()
-          print(f'Terkirim ke {ip}')
+          print(f'Terkirim ke {peer}')
         except Exception as e:
-          print(f'Gagal mengirim {nama_cc}.tar.gz ke {ip}: {e}')
+          print(f'Gagal mengirim {nama_cc}.tar.gz ke {peer}: {e}')
 
 if __name__ == "__main__":
-  file_rekom = "rekom_cc-peer.txt"
+  file_rekom = "rekom_cc_peer.txt"
   ssh_kunci = os.path.expanduser('/home/loadbalancer/.ssh/id_rsa')
 
   kirim_cc_sesuai_rekom(file_rekom, ssh_kunci)
--- src/read_pack_cc.py
+++ src/read_pack_cc.py
@@ -26,12 +26,6 @@ def baca_konten_file(alamat_file):
   with open(alamat_file, 'r') as f:
     return f.read()
 
-def alamat_lngkp_entri(alamat_dir):
-  alamat = []
-  for item in os.listdir(alamat_dir):
-    alamat_lngkp = os.path.join(alamat_dir, item)
-    alamat.append(alamat_lngkp)
-  return alamat
 
 def daftar_folder_chaincode(alamat_dir):
   folders = []
@@ -39,49 +33,15 @@ def daftar_folder_chaincode(alamat_dir):
     alamat_lngkp = os.path.join(alamat_dir, item)
 
     if os.path.isdir(alamat_lngkp):
-      # kemas folder chaincode
       kemas_chaincode(alamat_lngkp, item)
-
-      # baca entri dalam folder terbaca
-      # entri = alamat_lngkp_entri(alamat_lngkp)
-      # print(f"[Isi (file/folder) dalam {item}]")
-      # for e in entri:
-      #   print(e)
-
-      # print()
       folders.append(item)
 
   return folders
 
-def daftar_file_chaincode(alamat_dir):
-  files = []
-  for item in os.listdir(alamat_dir):
-    alamat_lngkp = os.path.join(alamat_dir, item)
-
-    if os.path.isfile(alamat_lngkp):
-      # isi_file = baca_konten_file(alamat_lngkp)
-      # print(f"[{item}]\n{isi_file}")
-      files.append(item)
-
-  return files
-
 if __name__ == "__main__":
   folder_chaincode = "../chaincodes"
 
-  # print()
-  # daftar_file = daftar_file_chaincode(folder_chaincode)
-  # print(f"[File dalam folder {folder_chaincode}]")
-  # for fi in daftar_file:
-  #   print(fi)
-  
-  print()
   daftar_folder = daftar_folder_chaincode(folder_chaincode)
   print(f"[Folder dalam folder {folder_chaincode}]")
   for fo in daftar_folder:
     print(fo)
-  
-  # print()
-  # entri = alamat_lngkp_entri(folder_chaincode)
-  # print("[Alamat lengkap entri (file/folder) chaincode]")
-  # for e in entri:
-  #   print(e)
