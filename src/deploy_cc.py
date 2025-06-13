a19f5a482879bfe97f3288f4842b24f39a29f1bf
SanGit56 <radhiyanrmhhisan@gmail.com>
2025-06-13 07:30:56 +0700

    memperbaiki indentasi

--- src/deploy_cc.py
+++ src/deploy_cc.py
@@ -3,20 +3,20 @@ import time
 import os
 
 def sebar_chaincode(directory):
-for nama_file in os.listdir(directory):
-	if nama_file.endswith(".tar.gz"):
-		nama_cc = nama_file[:-7]
-		waktu_mulai = time.time()
+	for nama_file in os.listdir(directory):
+		if nama_file.endswith(".tar.gz"):
+			nama_cc = nama_file[:-7]
+			waktu_mulai = time.time()
 
-		try:
-			subprocess.run(["scripts/deployCC.sh", "kanal-fabric", nama_cc], check=True)
-			print(f"Berhasil: {nama_cc}")
-		except subprocess.CalledProcessError as e:
-			print(f"Gagal {nama_cc}: {e}")
-	
-		waktu_selesai = time.time()
-		durasi = waktu_selesai - waktu_mulai
-		print(f"Durasi penyebaran {nama_cc}: {durasi:.2f} detik")
+			try:
+				subprocess.run(["scripts/deployCC.sh", "kanal-fabric", nama_cc], check=True)
+				print(f"Berhasil: {nama_cc}")
+			except subprocess.CalledProcessError as e:
+				print(f"Gagal {nama_cc}: {e}")
+		
+			waktu_selesai = time.time()
+			durasi = waktu_selesai - waktu_mulai
+			print(f"Durasi penyebaran {nama_cc}: {durasi:.2f} detik")
 
 if __name__ == "__main__":
 	waktu_mulai = time.time()
